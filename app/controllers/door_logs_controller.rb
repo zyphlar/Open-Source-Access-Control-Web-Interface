152d6e4e033e61b82306ff34b56112231670e948
Will Bradley <bradley.will@gmail.com>
2012-09-16 02:53:45 -0700

    Got auto-detection in logs working, improved user index readability

--- app/controllers/door_logs_controller.rb
+++ app/controllers/door_logs_controller.rb
@@ -15,17 +15,6 @@ class DoorLogsController < ApplicationController
 
   # GET /door_logs/1
   # GET /door_logs/1.json
-#  def show
-#    @door_log = DoorLog.find(params[:id])
-#
-#    respond_to do |format|
-#      format.html # show.html.erb
-#      format.json { render :json => @door_log }
-#    end
-#  end
-
-  # GET /door_logs/1
-  # GET /door_logs/1.json
   def download
     @results = DoorLog.download_from_door
 
@@ -35,63 +24,4 @@ class DoorLogsController < ApplicationController
     end
   end
 
-#  # GET /door_logs/new
-#  # GET /door_logs/new.json
-#  def new
-#    @door_log = DoorLog.new
-#
-#    respond_to do |format|
-#      format.html # new.html.erb
-#      format.json { render :json => @door_log }
-#    end
-#  end
-
-  # GET /door_logs/1/edit
-#  def edit
-#    @door_log = DoorLog.find(params[:id])
-#  end
-
-  # POST /door_logs
-  # POST /door_logs.json
-#  def create
-#    @door_log = DoorLog.new(params[:door_log])
-#
-#    respond_to do |format|
-#      if @door_log.save
-#        format.html { redirect_to @door_log, :notice => 'Door log was successfully created.' }
-#        format.json { render :json => @door_log, :status => :created, :location => @door_log }
-#      else
-#        format.html { render :action => "new" }
-#        format.json { render :json => @door_log.errors, :status => :unprocessable_entity }
-#      end
-#    end
-#  end
-
-  # PUT /door_logs/1
-  # PUT /door_logs/1.json
-#  def update
-#    @door_log = DoorLog.find(params[:id])
-#
-#    respond_to do |format|
-#      if @door_log.update_attributes(params[:door_log])
-#        format.html { redirect_to @door_log, :notice => 'Door log was successfully updated.' }
-#        format.json { head :no_content }
-#      else
-#        format.html { render :action => "edit" }
-#        format.json { render :json => @door_log.errors, :status => :unprocessable_entity }
-#      end
-#    end
-#  end
-
-  # DELETE /door_logs/1
-  # DELETE /door_logs/1.json
-#  def destroy
-#    @door_log = DoorLog.find(params[:id])
-#    @door_log.destroy
-#
-#    respond_to do |format|
-#      format.html { redirect_to door_logs_url }
-#      format.json { head :no_content }
-#    end
-#  end
 end
