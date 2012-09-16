f3443a3a82a87af1db04957248596ffe83970dc8
Will Bradley <bradley.will@gmail.com>
2012-09-15 21:44:06 -0700

    Fixed merge conflict with mailer url, need to remove this from git as well

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,6 @@ Dooraccess::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-<<<<<<< HEAD
-=======
   
   config.action_mailer.default_url_options = { :host => 'members.heatsynclabs.org' }
->>>>>>> 03d99741e5b661e63f6281115d2db333082a09b1
 end
