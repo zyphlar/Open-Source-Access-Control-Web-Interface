0b5978d4ef63a46788906bf5626aef0bc431e93d
Matthew Shepard <mattshepard@gmail.com>
2012-10-16 22:59:35 -0700

    Add active status to user show/edit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :admin, :active
 
   has_many :cards
 end
