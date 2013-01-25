95374a8c1fa47dbb8b2efadfcf8b4f20ea6abc2a
Will Bradley <bradley.will@gmail.com>
2013-01-25 08:45:43 -0700

    Moving password field and fixing a nil error in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|  
-    if current_user.orientation.blank? then
+    if !current_user.nil? && current_user.orientation.blank? then
       flash[:alert] = "Sorry, you need to complete New Member Orientation before having access to this page. <br/>Please check your email and schedule a New Member Orientation with a volunteer."
       redirect_to root_url
     end
