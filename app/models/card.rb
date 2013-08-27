18bc427eb3dc578756d0980866a84925ad196945
Will Bradley <bradley.will@gmail.com>
2013-08-26 23:01:43 -0700

    Adding quick-add buttons to user profiles

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,8 @@ class Card < ActiveRecord::Base
   require 'open-uri'
 
   attr_accessible :id, :user_id, :name, :card_number, :card_permissions
-  validates_uniqueness_of :id,:card_number
+  validates_presence_of :user_id, :card_number, :card_permissions
+  validates_uniqueness_of :id, :card_number
   belongs_to :user
 
   def upload_to_door
