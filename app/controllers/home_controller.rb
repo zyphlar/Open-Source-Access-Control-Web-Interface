4da2ec9463569415621c6ccae3cd7b6e92e96917
Will Bradley <bradley.will@gmail.com>
2013-09-28 20:17:38 -0700

    Tweaking main page stats to show member counts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,11 @@ def index
   @recent_certs = UserCertification.where("created_at > ?", DateTime.now - 7.days).count
   @num_users = User.count
   @recent_users = User.where("created_at > ?", DateTime.now - 7.days).count
+  # Payments: member levels are multipled by 10 to indicate current payment; 25 x 10 = 250
   @num_paid_users = User.all.select{|u| u.member_status >= 250 }.count
+  @num_plus_users = User.all.select{|u| u.member_status == 1000 }.count
+  @num_basic_users = User.all.select{|u| u.member_status == 500 }.count
+  @num_associate_users = User.all.select{|u| u.member_status == 250 }.count
   @num_delinquent_users = User.all.select{|u| !u.payment_status }.count
   if can? :read, User then
     @recent_user_names = User.where("member_level > 10").accessible_by(current_ability).order('created_at desc').limit(5)
@@ -14,7 +18,7 @@ def index
   @today_door_opens = DoorLog.where("key = 'G' AND created_at > ?", DateTime.now - 1.day).count
   @recent_door_opens = DoorLog.where("key = 'G' AND created_at > ?", DateTime.now - 7.days).count
   @num_door_denieds = DoorLog.where("key = 'D'").count
-  @recent_door_denieds = DoorLog.where("key = 'D' AND created_at > ?", DateTime.now - 7.days).count
+  @recent_door_denieds = DoorLog.where("key = 'D' AND created_at > ?", DateTime.now - 1.month).count
   @num_logins = User.sum('sign_in_count')
   @recent_logins = User.where('current_sign_in_at > ?',Date.today - 7.days).count
   @num_macs = Mac.count
