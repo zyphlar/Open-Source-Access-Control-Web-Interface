57a5a00352130da35bc323e20355c82e86407a97
Will Bradley <bradley.will@gmail.com>
2014-04-14 18:09:02 -0700

    Adding graph to door log (open/closed)

--- app/controllers/door_logs_controller.rb
+++ app/controllers/door_logs_controller.rb
@@ -5,7 +5,35 @@ class DoorLogsController < ApplicationController
   # GET /door_logs
   # GET /door_logs.json
   def index
-    @door_logs = DoorLog.find(:all, :order => "created_at DESC", :limit => 500)
+    @door_logs = DoorLog.find(:all, :order => "created_at DESC", :limit => 100)
+
+
+    begin
+      @start_date = DateTime.parse(params[:start])
+      @end_date = DateTime.parse(params[:end])
+    rescue
+      @start_date = DateTime.now - 2.weeks
+      @end_date = DateTime.now
+    end
+
+    @door_logs_by_hour = DoorLog.where("created_at > ? AND created_at < ? AND (key = ? OR key = ?)", @start_date, @end_date,"door_1_locked","door_2_locked").order("created_at ASC").group_by(&:key)
+    @door_log_graph = [
+      @door_logs_by_hour["door_1_locked"].map{|d| [d.created_at.to_time.to_i*1000, 1^d.data.to_i]}, # use XOR to invert 1 into 0 and vice versa
+      @door_logs_by_hour["door_2_locked"].map{|d| [d.created_at.to_time.to_i*1000, 1^d.data.to_i]}
+    ]
+
+    #  @door_logs_by_hour.each do |door_log|
+    #    # Add one computer for activate, subtract one for deactivate
+    #    if door_log.data == 1
+    #      
+    #    elsif door_log.data == 0
+    #      mac_running_balance -= 1
+    #    end
+
+    #    @door_log_graph << [time.to_time.to_i*1000,mac_running_balance]
+    #  end
+
+
 
     respond_to do |format|
       format.html # index.html.erb
