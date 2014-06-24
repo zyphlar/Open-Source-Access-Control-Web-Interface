1d6699ae9f8dc87111d8ed333e333129f907002b
Will Bradley <bradley.will@gmail.com>
2014-06-24 12:20:17 -0700

    Increasing visible detail of mac log

--- app/controllers/mac_logs_controller.rb
+++ app/controllers/mac_logs_controller.rb
@@ -4,6 +4,10 @@ before_filter :authenticate_user!
 
 def index
   @mac_logs = MacLog.desc.limit(1000)
+  @macs = {}
+  @mac_logs.each do |log|
+    @macs.merge!({log.mac => Mac.find_by_mac(log.mac)})
+  end
 end
 
 end
